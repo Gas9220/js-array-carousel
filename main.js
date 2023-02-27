d0312e405258696d97c2a8faee4ec8668c2b10e2
Gaspare <Gas9220@users.noreply.github.com>
2023-02-27 18:27:20 +0100

    Completato Bonus 2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // Riferimenti HTML
 const imgCol = document.querySelector('.col-lg');
+const thumbnails = document.querySelector('.thumbnails');
 const previousBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 
@@ -13,18 +14,24 @@ let actualImgIndex = 0;
 
 // Creo la variabile che conterrà l'html delle img
 let carouselContent = "";
+let thumbnailsContent = "";
 
 // Creo l'HTML per ogni immagine
 for (let i = 0; i < images.length; i++) {
+    thumbnailsContent += `<img src="img/0${i + 1}.webp" alt="thumbnail" class="thumbnail grayed"></img>`
     carouselContent += `<img src="img/0${i + 1}.webp" alt="carusel-photo" class="carusel-photo d-none">`;
 }
 
 // Assegno a img col il contenuto di carouselContent
 imgCol.innerHTML = carouselContent;
+thumbnails.innerHTML = thumbnailsContent
 
 // Prendo tutte le immagini che hanno la classe .carusel-photo e alla prima rimuovo il display none
 const carouselImgs = document.querySelectorAll('.carusel-photo');
 carouselImgs[actualImgIndex].classList.remove('d-none');
+
+const thumbnailsImgs = document.querySelectorAll('.thumbnail');
+thumbnailsImgs[actualImgIndex].classList.remove('grayed')
 
 // Rispondo al click del btn next
 nextBtn.addEventListener('click',
@@ -32,12 +39,16 @@ nextBtn.addEventListener('click',
         // Mi assicuro di poter aumentare il valore di actualImgIndex in base al numero delle immagini
         if (actualImgIndex < carouselImgs.length - 1) {
             carouselImgs[actualImgIndex].classList.add('d-none');
+            thumbnailsImgs[actualImgIndex].classList.add('grayed')
             actualImgIndex++;
             carouselImgs[actualImgIndex].classList.remove('d-none');
+            thumbnailsImgs[actualImgIndex].classList.remove('grayed')
         } else {
             carouselImgs[actualImgIndex].classList.add('d-none');
+            thumbnailsImgs[actualImgIndex].classList.add('grayed')
             actualImgIndex = 0; // Porto l'indice alla prima immagine
             carouselImgs[actualImgIndex].classList.remove('d-none');
+            thumbnailsImgs[actualImgIndex].classList.remove('grayed')
         }
     }
 )
@@ -48,12 +59,16 @@ previousBtn.addEventListener('click',
         // Mi assicuro di poter diminuire il valore di actualImgIndex in modo da fermarmi alla prima immagine
         if (actualImgIndex > 0) {
             carouselImgs[actualImgIndex].classList.add('d-none');
+            thumbnailsImgs[actualImgIndex].classList.add('grayed')
             actualImgIndex--;
             carouselImgs[actualImgIndex].classList.remove('d-none');
+            thumbnailsImgs[actualImgIndex].classList.remove('grayed')
         } else {
             carouselImgs[actualImgIndex].classList.add('d-none');
+            thumbnailsImgs[actualImgIndex].classList.add('grayed')
             actualImgIndex = carouselImgs.length -1; // Porto l'indice all'ultima immagine
             carouselImgs[actualImgIndex].classList.remove('d-none');
+            thumbnailsImgs[actualImgIndex].classList.remove('grayed')
         }
     }
 )
